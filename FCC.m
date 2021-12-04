d2bed5b252a1ebf55bc302261a3bbe4cf1796a67
Yunpeng Shi <yunpengs@princeton.edu>
2021-12-03 22:36:49 -0500

    Update FCC.m

--- FCC.m
+++ FCC.m
@@ -7,7 +7,8 @@
 
 %% X: (i,j)-th block of XMat (denoted as Xij) is the matching matrix encoding the keypoint
 %%    matches between i-th and j-th images. For example, Xij(k,l)=1 if and only if the k-th keypoint 
-%%    in image i is matched to the l-th keypoint in image
+%%    in image i is matched to the l-th keypoint in image. X is not necessarily binary. 
+%%    In principle, one can also use SIFT similarity matrix, though not tested in the paper.
 %% mat_size: a vector of length n, whose i-th element is the number of keypoints in camera i
 %% path_length: default =2; count paths that connects keypoints i,k in 2 steps, and j,k in 2 steps 
 %%              (longer paths are not needed as we use message-pssing)
